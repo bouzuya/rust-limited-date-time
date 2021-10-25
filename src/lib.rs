0fe0a77918fd26337a2e64ea8c9d256bf2053ac2
bouzuya <m@bouzuya.net>
2021-10-25 23:04:55 +0900

    copy from date-time: 0.11.1 8794aff

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+mod date;
+mod date_time;
+mod duration;
+mod instant;
+mod offset_date_time;
+mod ordinal_date;
+mod private;
+mod time;
+mod time_zone_offset;
+
+pub use self::date::*;
+pub use self::date_time::*;
+pub use self::duration::*;
+pub use self::instant::*;
+pub use self::offset_date_time::*;
+pub use self::ordinal_date::*;
+pub use self::time::*;
+pub use self::time_zone_offset::*;
