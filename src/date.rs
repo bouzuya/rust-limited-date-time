34c965e2a82f402327d1a755f3f255415b597a52
bouzuya <m@bouzuya.net>
2021-10-31 13:04:01 +0900

    move ordinal_date -> date::ordinal_date

--- src/date.rs
+++ src/date.rs
@@ -1,12 +1,14 @@
 mod day_of_month;
 mod day_of_year;
 mod month;
+mod ordinal_date;
 mod year;
 mod year_month;
 
 pub use self::day_of_month::{DayOfMonth, ParseDayOfMonthError};
 pub use self::day_of_year::*;
 pub use self::month::{Month, ParseMonthError};
+pub use self::ordinal_date::*;
 pub use self::year::{ParseYearError, Year};
 pub use self::year_month::{ParseYearMonthError, YearMonth};
 use thiserror::Error;
