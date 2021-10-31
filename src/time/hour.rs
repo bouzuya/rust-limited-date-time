7afe032f4edf193d8fe1e022152bd181e5bbfde5
bouzuya <m@bouzuya.net>
2021-10-31 18:21:14 +0900

    add convertion between hour and number

--- src/time/hour.rs
+++ src/time/hour.rs
@@ -59,9 +59,87 @@ impl std::str::FromStr for Hour {
     }
 }
 
+impl From<Hour> for i8 {
+    fn from(hour: Hour) -> Self {
+        i8::try_from(hour.0).expect("hour is [0, 23]")
+    }
+}
+
+impl From<Hour> for i16 {
+    fn from(hour: Hour) -> Self {
+        i16::from(hour.0)
+    }
+}
+
+impl From<Hour> for i32 {
+    fn from(hour: Hour) -> Self {
+        i32::from(hour.0)
+    }
+}
+
+impl From<Hour> for i64 {
+    fn from(hour: Hour) -> Self {
+        i64::from(hour.0)
+    }
+}
+
 impl From<Hour> for u8 {
     fn from(hour: Hour) -> Self {
         hour.0
+    }
+}
+
+impl From<Hour> for u16 {
+    fn from(hour: Hour) -> Self {
+        u16::from(hour.0)
+    }
+}
+
+impl From<Hour> for u32 {
+    fn from(hour: Hour) -> Self {
+        u32::from(hour.0)
+    }
+}
+
+impl From<Hour> for u64 {
+    fn from(hour: Hour) -> Self {
+        u64::from(hour.0)
+    }
+}
+
+impl std::convert::TryFrom<i8> for Hour {
+    type Error = TryFromHourError;
+
+    fn try_from(value: i8) -> Result<Self, Self::Error> {
+        let value_as_u8 = u8::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        Self::try_from(value_as_u8)
+    }
+}
+
+impl std::convert::TryFrom<i16> for Hour {
+    type Error = TryFromHourError;
+
+    fn try_from(value: i16) -> Result<Self, Self::Error> {
+        let value_as_u8 = u8::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        Self::try_from(value_as_u8)
+    }
+}
+
+impl std::convert::TryFrom<i32> for Hour {
+    type Error = TryFromHourError;
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        let value_as_u8 = u8::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        Self::try_from(value_as_u8)
+    }
+}
+
+impl std::convert::TryFrom<i64> for Hour {
+    type Error = TryFromHourError;
+
+    fn try_from(value: i64) -> Result<Self, Self::Error> {
+        let value_as_u8 = u8::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        Self::try_from(value_as_u8)
     }
 }
 
@@ -73,6 +151,33 @@ impl std::convert::TryFrom<u8> for Hour {
             return Err(Self::Error::OutOfRange);
         }
         Ok(Self(value))
+    }
+}
+
+impl std::convert::TryFrom<u16> for Hour {
+    type Error = TryFromHourError;
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        let value_as_u8 = u8::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        Self::try_from(value_as_u8)
+    }
+}
+
+impl std::convert::TryFrom<u32> for Hour {
+    type Error = TryFromHourError;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        let value_as_u8 = u8::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        Self::try_from(value_as_u8)
+    }
+}
+
+impl std::convert::TryFrom<u64> for Hour {
+    type Error = TryFromHourError;
+
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        let value_as_u8 = u8::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        Self::try_from(value_as_u8)
     }
 }
 
@@ -109,11 +214,78 @@ mod tests {
     }
 
     #[test]
+    fn i8_conversion_test() {
+        type E = TryFromHourError;
+        let f = |d: i8| Hour::try_from(d);
+        assert_eq!(f(-1_i8), Err(E::OutOfRange));
+        assert_eq!(f(0_i8).map(i8::from), Ok(0_i8));
+        assert_eq!(f(23_i8).map(i8::from), Ok(23_i8));
+        assert_eq!(f(24_i8), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn i16_conversion_test() {
+        type E = TryFromHourError;
+        let f = |d: i16| Hour::try_from(d);
+        assert_eq!(f(-1_i16), Err(E::OutOfRange));
+        assert_eq!(f(0_i16).map(i16::from), Ok(0_i16));
+        assert_eq!(f(23_i16).map(i16::from), Ok(23_i16));
+        assert_eq!(f(24_i16), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn i32_conversion_test() {
+        type E = TryFromHourError;
+        let f = |d: i32| Hour::try_from(d);
+        assert_eq!(f(-1_i32), Err(E::OutOfRange));
+        assert_eq!(f(0_i32).map(i32::from), Ok(0_i32));
+        assert_eq!(f(23_i32).map(i32::from), Ok(23_i32));
+        assert_eq!(f(24_i32), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn i64_conversion_test() {
+        type E = TryFromHourError;
+        let f = |d: i64| Hour::try_from(d);
+        assert_eq!(f(-1_i64), Err(E::OutOfRange));
+        assert_eq!(f(0_i64).map(i64::from), Ok(0_i64));
+        assert_eq!(f(23_i64).map(i64::from), Ok(23_i64));
+        assert_eq!(f(24_i64), Err(E::OutOfRange));
+    }
+
+    #[test]
     fn u8_conversion_test() {
         type E = TryFromHourError;
         let f = |d: u8| Hour::try_from(d);
         assert_eq!(f(0_u8).map(u8::from), Ok(0_u8));
         assert_eq!(f(23_u8).map(u8::from), Ok(23_u8));
         assert_eq!(f(24_u8), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn u16_conversion_test() {
+        type E = TryFromHourError;
+        let f = |d: u16| Hour::try_from(d);
+        assert_eq!(f(0_u16).map(u16::from), Ok(0_u16));
+        assert_eq!(f(23_u16).map(u16::from), Ok(23_u16));
+        assert_eq!(f(24_u16), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn u32_conversion_test() {
+        type E = TryFromHourError;
+        let f = |d: u32| Hour::try_from(d);
+        assert_eq!(f(0_u32).map(u32::from), Ok(0_u32));
+        assert_eq!(f(23_u32).map(u32::from), Ok(23_u32));
+        assert_eq!(f(24_u32), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn u64_conversion_test() {
+        type E = TryFromHourError;
+        let f = |d: u64| Hour::try_from(d);
+        assert_eq!(f(0_u64).map(u64::from), Ok(0_u64));
+        assert_eq!(f(23_u64).map(u64::from), Ok(23_u64));
+        assert_eq!(f(24_u64), Err(E::OutOfRange));
     }
 }
