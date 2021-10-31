8404b3baa062436231b297a0ea245b782aac79f9
bouzuya <m@bouzuya.net>
2021-10-31 16:04:21 +0900

    add convertion between day_of_year and number

--- src/date/day_of_year.rs
+++ src/date/day_of_year.rs
@@ -3,6 +3,7 @@ use thiserror::Error;
 
 use crate::Days;
 
+// [1, 366]
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct DayOfYear(u16);
 
@@ -85,9 +86,100 @@ impl std::str::FromStr for DayOfYear {
     }
 }
 
+impl From<DayOfYear> for i16 {
+    fn from(day_of_year: DayOfYear) -> Self {
+        i16::try_from(day_of_year.0).expect("day_of_year is [1,366]")
+    }
+}
+
+impl From<DayOfYear> for i32 {
+    fn from(day_of_year: DayOfYear) -> Self {
+        i32::from(day_of_year.0)
+    }
+}
+
+impl From<DayOfYear> for i64 {
+    fn from(day_of_year: DayOfYear) -> Self {
+        i64::from(day_of_year.0)
+    }
+}
+
 impl From<DayOfYear> for u16 {
-    fn from(day_of_month: DayOfYear) -> Self {
-        day_of_month.0
+    fn from(day_of_year: DayOfYear) -> Self {
+        day_of_year.0
+    }
+}
+
+impl From<DayOfYear> for u32 {
+    fn from(day_of_year: DayOfYear) -> Self {
+        u32::from(day_of_year.0)
+    }
+}
+
+impl From<DayOfYear> for u64 {
+    fn from(day_of_year: DayOfYear) -> Self {
+        u64::from(day_of_year.0)
+    }
+}
+
+impl std::convert::TryFrom<DayOfYear> for i8 {
+    type Error = TryFromDayOfYearError;
+
+    fn try_from(value: DayOfYear) -> Result<Self, Self::Error> {
+        i8::try_from(value.0).map_err(|_| Self::Error::OutOfRange)
+    }
+}
+
+impl std::convert::TryFrom<DayOfYear> for u8 {
+    type Error = TryFromDayOfYearError;
+
+    fn try_from(value: DayOfYear) -> Result<Self, Self::Error> {
+        u8::try_from(value.0).map_err(|_| Self::Error::OutOfRange)
+    }
+}
+
+impl std::convert::TryFrom<i8> for DayOfYear {
+    type Error = TryFromDayOfYearError;
+
+    fn try_from(value: i8) -> Result<Self, Self::Error> {
+        let value_as_u16 = u16::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        DayOfYear::try_from(value_as_u16)
+    }
+}
+
+impl std::convert::TryFrom<i16> for DayOfYear {
+    type Error = TryFromDayOfYearError;
+
+    fn try_from(value: i16) -> Result<Self, Self::Error> {
+        let value_as_u16 = u16::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        DayOfYear::try_from(value_as_u16)
+    }
+}
+
+impl std::convert::TryFrom<i32> for DayOfYear {
+    type Error = TryFromDayOfYearError;
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        let value_as_u16 = u16::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        DayOfYear::try_from(value_as_u16)
+    }
+}
+
+impl std::convert::TryFrom<i64> for DayOfYear {
+    type Error = TryFromDayOfYearError;
+
+    fn try_from(value: i64) -> Result<Self, Self::Error> {
+        let value_as_u16 = u16::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        DayOfYear::try_from(value_as_u16)
+    }
+}
+
+impl std::convert::TryFrom<u8> for DayOfYear {
+    type Error = TryFromDayOfYearError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        let value_as_u16 = u16::from(value);
+        DayOfYear::try_from(value_as_u16)
     }
 }
 
@@ -99,6 +191,24 @@ impl std::convert::TryFrom<u16> for DayOfYear {
             return Err(Self::Error::OutOfRange);
         }
         Ok(Self(value))
+    }
+}
+
+impl std::convert::TryFrom<u32> for DayOfYear {
+    type Error = TryFromDayOfYearError;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        let value_as_u16 = u16::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        DayOfYear::try_from(value_as_u16)
+    }
+}
+
+impl std::convert::TryFrom<u64> for DayOfYear {
+    type Error = TryFromDayOfYearError;
+
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        let value_as_u16 = u16::try_from(value).map_err(|_| Self::Error::OutOfRange)?;
+        DayOfYear::try_from(value_as_u16)
     }
 }
 
@@ -143,6 +253,54 @@ mod tests {
     }
 
     #[test]
+    fn i8_conversion_test() {
+        type E = TryFromDayOfYearError;
+        let f = |d: i8| DayOfYear::try_from(d);
+        assert_eq!(f(0_i8), Err(E::OutOfRange));
+        assert_eq!(f(1_i8).and_then(i8::try_from), Ok(1_i8));
+        assert_eq!(f(i8::MAX).and_then(i8::try_from), Ok(127_i8));
+    }
+
+    #[test]
+    fn i16_conversion_test() {
+        type E = TryFromDayOfYearError;
+        let f = |d: i16| DayOfYear::try_from(d);
+        assert_eq!(f(0_i16), Err(E::OutOfRange));
+        assert_eq!(f(1_i16).map(i16::from), Ok(1_i16));
+        assert_eq!(f(366_i16).map(i16::from), Ok(366_i16));
+        assert_eq!(f(367_i16), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn i32_conversion_test() {
+        type E = TryFromDayOfYearError;
+        let f = |d: i32| DayOfYear::try_from(d);
+        assert_eq!(f(0_i32), Err(E::OutOfRange));
+        assert_eq!(f(1_i32).map(i32::from), Ok(1_i32));
+        assert_eq!(f(366_i32).map(i32::from), Ok(366_i32));
+        assert_eq!(f(367_i32), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn i64_conversion_test() {
+        type E = TryFromDayOfYearError;
+        let f = |d: i64| DayOfYear::try_from(d);
+        assert_eq!(f(0_i64), Err(E::OutOfRange));
+        assert_eq!(f(1_i64).map(i64::from), Ok(1_i64));
+        assert_eq!(f(366_i64).map(i64::from), Ok(366_i64));
+        assert_eq!(f(367_i64), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn u8_conversion_test() {
+        type E = TryFromDayOfYearError;
+        let f = |d: u8| DayOfYear::try_from(d);
+        assert_eq!(f(0_u8), Err(E::OutOfRange));
+        assert_eq!(f(1_u8).and_then(u8::try_from), Ok(1_u8));
+        assert_eq!(f(u8::MAX).and_then(u8::try_from), Ok(255_u8));
+    }
+
+    #[test]
     fn u16_conversion_test() {
         type E = TryFromDayOfYearError;
         let f = |d: u16| DayOfYear::try_from(d);
@@ -150,6 +308,26 @@ mod tests {
         assert_eq!(f(1_u16).map(u16::from), Ok(1_u16));
         assert_eq!(f(366_u16).map(u16::from), Ok(366_u16));
         assert_eq!(f(367_u16), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn u32_conversion_test() {
+        type E = TryFromDayOfYearError;
+        let f = |d: u32| DayOfYear::try_from(d);
+        assert_eq!(f(0_u32), Err(E::OutOfRange));
+        assert_eq!(f(1_u32).map(u32::from), Ok(1_u32));
+        assert_eq!(f(366_u32).map(u32::from), Ok(366_u32));
+        assert_eq!(f(367_u32), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn u64_conversion_test() {
+        type E = TryFromDayOfYearError;
+        let f = |d: u64| DayOfYear::try_from(d);
+        assert_eq!(f(0_u64), Err(E::OutOfRange));
+        assert_eq!(f(1_u64).map(u64::from), Ok(1_u64));
+        assert_eq!(f(366_u64).map(u64::from), Ok(366_u64));
+        assert_eq!(f(367_u64), Err(E::OutOfRange));
     }
 
     #[test]
