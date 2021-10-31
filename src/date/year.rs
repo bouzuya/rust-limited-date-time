e75680089a966233cffacb07d2d886a166fc5587
bouzuya <m@bouzuya.net>
2021-10-31 15:04:17 +0900

    add convertion between year and number

--- src/date/year.rs
+++ src/date/year.rs
@@ -87,9 +87,66 @@ impl std::str::FromStr for Year {
     }
 }
 
+impl From<Year> for i16 {
+    fn from(year: Year) -> Self {
+        i16::try_from(year.0).expect("year.0 is [1970,9999]")
+    }
+}
+
+impl From<Year> for i32 {
+    fn from(year: Year) -> Self {
+        i32::from(year.0)
+    }
+}
+
+impl From<Year> for i64 {
+    fn from(year: Year) -> Self {
+        i64::from(year.0)
+    }
+}
+
 impl From<Year> for u16 {
     fn from(year: Year) -> Self {
         year.0
+    }
+}
+
+impl From<Year> for u32 {
+    fn from(year: Year) -> Self {
+        u32::from(year.0)
+    }
+}
+
+impl From<Year> for u64 {
+    fn from(year: Year) -> Self {
+        u64::from(year.0)
+    }
+}
+
+impl std::convert::TryFrom<i16> for Year {
+    type Error = TryFromYearError;
+
+    fn try_from(value: i16) -> Result<Self, Self::Error> {
+        let value_as_u16 = u16::try_from(value).map_err(|_| TryFromYearError::OutOfRange)?;
+        Year::try_from(value_as_u16)
+    }
+}
+
+impl std::convert::TryFrom<i32> for Year {
+    type Error = TryFromYearError;
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        let value_as_u16 = u16::try_from(value).map_err(|_| TryFromYearError::OutOfRange)?;
+        Year::try_from(value_as_u16)
+    }
+}
+
+impl std::convert::TryFrom<i64> for Year {
+    type Error = TryFromYearError;
+
+    fn try_from(value: i64) -> Result<Self, Self::Error> {
+        let value_as_u16 = u16::try_from(value).map_err(|_| TryFromYearError::OutOfRange)?;
+        Year::try_from(value_as_u16)
     }
 }
 
@@ -104,8 +161,28 @@ impl std::convert::TryFrom<u16> for Year {
     }
 }
 
+impl std::convert::TryFrom<u32> for Year {
+    type Error = TryFromYearError;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        let value_as_u16 = u16::try_from(value).map_err(|_| TryFromYearError::OutOfRange)?;
+        Year::try_from(value_as_u16)
+    }
+}
+
+impl std::convert::TryFrom<u64> for Year {
+    type Error = TryFromYearError;
+
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        let value_as_u16 = u16::try_from(value).map_err(|_| TryFromYearError::OutOfRange)?;
+        Year::try_from(value_as_u16)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use super::*;
 
     #[test]
@@ -131,6 +208,36 @@ mod tests {
     }
 
     #[test]
+    fn i16_conversion_test() {
+        type E = TryFromYearError;
+        let f = |y: i16| Year::try_from(y);
+        assert_eq!(f(1969_i16), Err(E::OutOfRange));
+        assert_eq!(f(1970_i16).map(i16::from), Ok(1970_i16));
+        assert_eq!(f(9999_i16).map(i16::from), Ok(9999_i16));
+        assert_eq!(f(10000_i16), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn i32_conversion_test() {
+        type E = TryFromYearError;
+        let f = |y: i32| Year::try_from(y);
+        assert_eq!(f(1969_i32), Err(E::OutOfRange));
+        assert_eq!(f(1970_i32).map(i32::from), Ok(1970_i32));
+        assert_eq!(f(9999_i32).map(i32::from), Ok(9999_i32));
+        assert_eq!(f(10000_i32), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn i64_conversion_test() {
+        type E = TryFromYearError;
+        let f = |y: i64| Year::try_from(y);
+        assert_eq!(f(1969_i64), Err(E::OutOfRange));
+        assert_eq!(f(1970_i64).map(i64::from), Ok(1970_i64));
+        assert_eq!(f(9999_i64).map(i64::from), Ok(9999_i64));
+        assert_eq!(f(10000_i64), Err(E::OutOfRange));
+    }
+
+    #[test]
     fn u16_conversion_test() {
         type E = TryFromYearError;
         let f = |y: u16| Year::try_from(y);
@@ -141,16 +248,39 @@ mod tests {
     }
 
     #[test]
+    fn u32_conversion_test() {
+        type E = TryFromYearError;
+        let f = |y: u32| Year::try_from(y);
+        assert_eq!(f(1969_u32), Err(E::OutOfRange));
+        assert_eq!(f(1970_u32).map(u32::from), Ok(1970_u32));
+        assert_eq!(f(9999_u32).map(u32::from), Ok(9999_u32));
+        assert_eq!(f(10000_u32), Err(E::OutOfRange));
+    }
+
+    #[test]
+    fn u64_conversion_test() {
+        type E = TryFromYearError;
+        let f = |y: u64| Year::try_from(y);
+        assert_eq!(f(1969_u64), Err(E::OutOfRange));
+        assert_eq!(f(1970_u64).map(u64::from), Ok(1970_u64));
+        assert_eq!(f(9999_u64).map(u64::from), Ok(9999_u64));
+        assert_eq!(f(10000_u64), Err(E::OutOfRange));
+    }
+
+    #[test]
     fn first_day_of_year_test() -> anyhow::Result<()> {
-        assert_eq!(Year::try_from(2000)?.first_day_of_year(), DayOfYear::min());
+        assert_eq!(
+            Year::from_str("2000")?.first_day_of_year(),
+            DayOfYear::min()
+        );
         Ok(())
     }
 
     #[test]
     fn last_day_of_year_test() -> anyhow::Result<()> {
-        let leap_year = Year::try_from(2000)?;
+        let leap_year = Year::from_str("2000")?;
         assert_eq!(leap_year.last_day_of_year(), DayOfYear::max_in_leap_year());
-        let common_year = Year::try_from(2001)?;
+        let common_year = Year::from_str("2001")?;
         assert_eq!(
             common_year.last_day_of_year(),
             DayOfYear::max_in_common_year()
@@ -160,24 +290,36 @@ mod tests {
 
     #[test]
     fn pred_test() -> anyhow::Result<()> {
-        assert_eq!(Year::try_from(9999)?.pred(), Some(Year::try_from(9998)?));
-        assert_eq!(Year::try_from(1971)?.pred(), Some(Year::try_from(1970)?));
-        assert_eq!(Year::try_from(1970)?.pred(), None);
+        assert_eq!(
+            Year::from_str("9999")?.pred(),
+            Some(Year::from_str("9998")?)
+        );
+        assert_eq!(
+            Year::from_str("1971")?.pred(),
+            Some(Year::from_str("1970")?)
+        );
+        assert_eq!(Year::from_str("1970")?.pred(), None);
         Ok(())
     }
 
     #[test]
     fn succ_test() -> anyhow::Result<()> {
-        assert_eq!(Year::try_from(1970)?.succ(), Some(Year::try_from(1971)?));
-        assert_eq!(Year::try_from(9998)?.succ(), Some(Year::try_from(9999)?));
-        assert_eq!(Year::try_from(9999)?.succ(), None);
+        assert_eq!(
+            Year::from_str("1970")?.succ(),
+            Some(Year::from_str("1971")?)
+        );
+        assert_eq!(
+            Year::from_str("9998")?.succ(),
+            Some(Year::from_str("9999")?)
+        );
+        assert_eq!(Year::from_str("9999")?.succ(), None);
         Ok(())
     }
 
     #[test]
     fn days_test() -> anyhow::Result<()> {
-        assert_eq!(Year::try_from(2000)?.days(), Days::from(366));
-        assert_eq!(Year::try_from(2001)?.days(), Days::from(365));
+        assert_eq!(Year::from_str("2000")?.days(), Days::from(366));
+        assert_eq!(Year::from_str("2001")?.days(), Days::from(365));
         Ok(())
     }
 }
